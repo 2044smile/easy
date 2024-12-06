0226310d1f198e85ac2a9ca66985494a8764fc7e
2044smile@naver.com <2044smile@naver.com>
2024-12-06 19:41:49 +0900

    ♻️ refactor: related secret

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -9,18 +9,33 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os, json
 from pathlib import Path
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
+# NAVER DEVELOPERS
+CLIENT_ID = get_secret("CLIENT_ID")
+CLIENT_SECRET = get_secret("CLIENT_SECRET")
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cku_uy7$_i130j+@z8ytkqm8oow+f25+no(a+yx0e1estl&&@l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
